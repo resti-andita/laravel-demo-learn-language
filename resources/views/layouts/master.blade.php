efa5b4d5e23de7f0d6a68912247a9011a8ab71c8
Kadek Wresti <8492andita@gmail.com>
2019-07-19 11:02:04 +0800

    Title get from config app.name

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>@yield('title') | <?php config('app.name'); ?></title>
+	<title>@yield('title') | {{ config('app.name') }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Language learning social network that connects students and language teachers." />
 	<meta name="keywords" content="" />
